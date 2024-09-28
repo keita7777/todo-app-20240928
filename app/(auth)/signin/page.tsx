240224dd03141c62e77bc230e10af7f8c20a3ee5
github keita7777 <github mine0kei@gmail.com>
2024-09-28 18:51:20 +0900

    signupページをもとにuseFormでフォームを定義

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,34 +1,70 @@
+"use client";
+
+import { loginSchema } from "@/validations/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export type LoginFormInput = z.infer<typeof loginSchema>;
+
 const Signin = () => {
+  const {
+    handleSubmit,
+    reset,
+    register,
+    setError,
+    formState,
+    formState: { errors },
+  } = useForm<LoginFormInput>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginFormInput) => {
+    console.log(data);
+  };
+
   return (
     <div>
       <h1 className="text-center text-2xl font-bold mb-6">ログイン</h1>
-      <form action="">
-        <div className="flex flex-col">
-          <label htmlFor="username">ユーザー名</label>
-          <input
-            id="username"
-            name="username"
-            type="text"
-            className="border rounded-md p-2 text-sm mb-2"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="password">パスワード</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            className="border rounded-md p-2 text-sm mb-2"
-          />
-        </div>
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white px-2 py-2 mt-4 rounded-md w-2/5"
-          >
-            ログイン
-          </button>
-        </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset disabled={formState.isSubmitting}>
+          <div className="flex flex-col">
+            <label htmlFor="username">ユーザー名</label>
+            <input
+              id="username"
+              type="text"
+              className="border rounded-md p-2 text-sm mb-2"
+              {...register("email")}
+            />
+            {errors["email"] && (
+              <p className="text-red-500">{errors["email"].message}</p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">パスワード</label>
+            <input
+              id="password"
+              type="password"
+              className="border rounded-md p-2 text-sm mb-2"
+              {...register("password")}
+            />
+            {errors["password"] && (
+              <p className="text-red-500">{errors["password"].message}</p>
+            )}
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-2 py-2 mt-4 rounded-md w-2/5"
+            >
+              ログイン
+            </button>
+          </div>
+        </fieldset>
       </form>
     </div>
   );
