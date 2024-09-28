1e87a6a92eeac0bc30d465ab4f5571976f7ac115
github keita7777 <github mine0kei@gmail.com>
2024-09-28 20:19:42 +0900

    providersにCredentialsの設定を記述

--- auth.ts
+++ auth.ts
@@ -1,5 +1,39 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./prisma/db";
+import { compare } from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) {
+          throw new Error("このメールアドレスは登録されていません");
+        } else {
+          const passwordCorrect = await compare(
+            credentials.password as string,
+            user.password!
+          );
+          if (!passwordCorrect) {
+            throw new Error("パスワードが間違っています");
+          }
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
+    }),
+  ],
 });
