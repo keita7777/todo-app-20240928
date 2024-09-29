8e496461387dc9b1b6558a9ef4722414ead7383b
github keita7777 <github mine0kei@gmail.com>
2024-09-29 11:29:34 +0900

    todoスキーマの作成

---
+++ validations/todoSchema.ts
@@ -0,0 +1,8 @@
+import { Status } from "@prisma/client";
+import { z } from "zod";
+
+export const todoSchema = z.object({
+  title: z.string().min(1, "タイトルを入力してください"),
+  description: z.string().min(1, "詳細を入力してください"),
+  status: z.enum(Object.keys(Status) as [Status, ...Status[]]),
+});
