d804a3e5b05453357c5ae14157869f44c11a635c
github keita7777 <github mine0kei@gmail.com>
2024-09-30 03:29:11 +0900

    ヘッダー開閉メニュー修正

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,4 +1,9 @@
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <p>ユーザー名：</p>
+      <p>最終ログイン日：202412月12日</p>
+    </div>
+  );
 };
 export default Profile;
--- components/header/ProfileButton.tsx
+++ components/header/ProfileButton.tsx
@@ -2,20 +2,42 @@
 
 import { FaUserCircle } from "react-icons/fa";
 import Link from "next/link";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+// https://katatumuri.xyz/react/357/react-modal-function-component/
+// このページを参考に作成
 
 const ProfileButton = () => {
   const [isModelOpen, setIsModelOpen] = useState(false);
+
+  // -----3------
+  const closeModal = useCallback(() => {
+    setIsModelOpen(false);
+    document.removeEventListener("click", closeModal);
+  }, []);
+  // ---↑修正----
+
+  // -----2------
+  useEffect(() => {
+    return () => {
+      document.removeEventListener("click", closeModal);
+    };
+  }, [closeModal]);
+  // ---↑追加----
+
+  function openModal(e: any) {
+    setIsModelOpen(true);
+    document.addEventListener("click", closeModal);
+    e.stopPropagation();
+  }
+
   return (
     <div className="relative">
       <button
-        onClick={(e) => {
+        onClick={(e: any) => {
           e.preventDefault();
-          setIsModelOpen((prev) => !prev);
-        }}
-        onBlur={(e) => {
-          e.preventDefault();
-          setIsModelOpen(false);
+          e.stopPropagation();
+          openModal(e);
         }}
       >
         <FaUserCircle />
@@ -23,11 +45,13 @@ const ProfileButton = () => {
       {isModelOpen && (
         <div className="absolute top-10 right-0 w-44 bg-blue-200 rounded-md p-2">
           <ul className="flex flex-col gap-2 text-sm">
-            <li className="bg-white rounded-md text-center p-2">
-              <Link href="/profile">プロフィール</Link>
+            <li className="bg-white rounded-md text-center">
+              <Link href="/profile" className="block p-2">
+                プロフィール
+              </Link>
             </li>
-            <li className="bg-white rounded-md text-center p-2">
-              <button>ログアウト</button>
+            <li className="bg-white rounded-md text-center">
+              <button className="block w-full p-2">ログアウト</button>
             </li>
           </ul>
         </div>
