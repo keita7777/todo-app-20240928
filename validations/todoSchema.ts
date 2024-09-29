291806d3d04073636dd4568c5d8f3f638fcb630e
github keita7777 <github mine0kei@gmail.com>
2024-09-29 11:36:48 +0900

    todoスキーマでstatusのエラーメッセージを定義

--- validations/todoSchema.ts
+++ validations/todoSchema.ts
@@ -4,5 +4,7 @@ import { z } from "zod";
 export const todoSchema = z.object({
   title: z.string().min(1, "タイトルを入力してください"),
   description: z.string().min(1, "詳細を入力してください"),
-  status: z.enum(Object.keys(Status) as [Status, ...Status[]]),
+  status: z.enum(Object.keys(Status) as [Status, ...Status[]], {
+    errorMap: () => ({ message: "ステータスを選択してください" }),
+  }),
 });
