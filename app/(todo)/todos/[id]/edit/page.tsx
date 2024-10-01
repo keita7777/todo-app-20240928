5fdc26d3723ac03f74ae1b473a7705efd149acb9
github keita7777 <github mine0kei@gmail.com>
2024-10-02 02:36:46 +0900

    ログインユーザーがTODO作成者でない場合は詳細ページにリダイレクトさせる

--- app/(todo)/todos/[id]/edit/page.tsx
+++ app/(todo)/todos/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
+import { auth } from "@/auth";
 import EditForm from "@/components/todo/EditForm";
 import { getTodoDetail } from "@/lib/actions";
 import { Status } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 export type EditTodo = {
   id: string;
@@ -11,6 +13,13 @@ export type EditTodo = {
 
 const Edit = async ({ params }: { params: { id: string } }) => {
   const todo = await getTodoDetail(params.id);
+  const session = await auth();
+  const currentUserEmail = session?.user?.email;
+
+  // ログインユーザーがTODO作成者でない場合は詳細ページにリダイレクトさせる
+  if (currentUserEmail !== todo.user.email) {
+    redirect(`/todos/${todo.id}`);
+  }
 
   return (
     <div>
