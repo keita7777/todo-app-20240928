89f2fa65a3b41a5e2fa8b5189fd54c2649b7da82
github keita7777 <github mine0kei@gmail.com>
2024-10-01 19:09:19 +0900

    リダイレクトテスト

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default function Home() {
   const session = auth();
