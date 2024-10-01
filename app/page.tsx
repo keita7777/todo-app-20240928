c19bf7d224327ef19349fa49587ae69ef94d73e7
github keita7777 <github mine0kei@gmail.com>
2024-10-01 19:08:24 +0900

    リダイレクトテスト

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import { redirect } from "next/navigation";
 export default function Home() {
   const session = auth();
   if (!session) {
-    redirect("/signin");
+    // redirect("/signin");
+    return <p>sessionがありません</p>;
   }
-  redirect("/todos");
+  // redirect("/todos");
+  return <p>sessionがあります</p>;
 }
 
 // ホーム画面作ってなかったのでリダイレクトさせるようにした
