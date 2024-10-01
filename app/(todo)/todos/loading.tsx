215a9a5971583b4ed1944ab2e66d3cac5501e139
github keita7777 <github mine0kei@gmail.com>
2024-10-01 16:51:43 +0900

    ローディング画面テスト

---
+++ app/(todo)/todos/loading.tsx
@@ -0,0 +1,8 @@
+const loading = () => {
+  return (
+    <div className="absolute top-0 left-0 w-screen h-screen bg-orange-100 bg-opacity-60 flex justify-center items-center">
+      <p className="text-2xl">ローディング中...</p>
+    </div>
+  );
+};
+export default loading;
