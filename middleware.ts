b105ea613a87eee899c0745b7c4d09e0e7484e95
github keita7777 <github mine0kei@gmail.com>
2024-10-01 15:45:46 +0900

    middlewareを修正

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
+import { auth } from "./auth";
 
-export function middleware(request: Request) {
-  return NextResponse.redirect(new URL("/signup", request.url));
+export async function middleware(request: Request) {
+  const session = auth();
+  if (!session) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+  return NextResponse.next();
 }
 
 export const config = {
