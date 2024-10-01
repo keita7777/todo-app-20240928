7844cc4e5eb1318a5fd9a70e4c2415bb751f5a38
github keita7777 <github mine0kei@gmail.com>
2024-10-01 14:23:01 +0900

    ログアウト状態でtodos。profileにアクセスするとsignupページにリダイレクトされるようにmiddlewareを設定

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,9 @@
-export { auth as middleware } from "@/auth";
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+  return NextResponse.redirect(new URL("/signup", request.url));
+}
+
+export const config = {
+  matcher: ["/todos/:path*", "/profile/:path*"],
+};
