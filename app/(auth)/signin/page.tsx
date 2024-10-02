3e5e514ebd04d91255bd30e5b6f1d995719bb98a
github keita7777 <github mine0kei@gmail.com>
2024-10-02 12:44:21 +0900

    送信完了後の表示を修正

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -43,43 +43,47 @@ const Signin = () => {
 
   return (
     <div>
-      <h1 className="text-center text-2xl font-bold mb-6">ログイン</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <fieldset disabled={formState.isSubmitting}>
-          <div className="flex flex-col mb-4">
-            <label htmlFor="username">ユーザー名</label>
-            <input
-              id="username"
-              type="text"
-              className="border rounded-md p-2 text-sm"
-              {...register("email")}
-            />
-            {errors["email"] && (
-              <p className="text-red-500">{errors["email"].message}</p>
-            )}
-          </div>
-          <div className="flex flex-col mb-4">
-            <label htmlFor="password">パスワード</label>
-            <input
-              id="password"
-              type="password"
-              className="border rounded-md p-2 text-sm"
-              {...register("password")}
-            />
-            {errors["password"] && (
-              <p className="text-red-500">{errors["password"].message}</p>
-            )}
-          </div>
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-2 py-2 mt-4 rounded-md w-2/5"
-            >
-              ログイン
-            </button>
-          </div>
-        </fieldset>
-      </form>
+      {formState.isSubmitSuccessful ? null : (
+        <>
+          <h1 className="text-center text-2xl font-bold mb-6">ログイン</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset disabled={formState.isSubmitting}>
+              <div className="flex flex-col mb-4">
+                <label htmlFor="username">ユーザー名</label>
+                <input
+                  id="username"
+                  type="text"
+                  className="border rounded-md p-2 text-sm"
+                  {...register("email")}
+                />
+                {errors["email"] && (
+                  <p className="text-red-500">{errors["email"].message}</p>
+                )}
+              </div>
+              <div className="flex flex-col mb-4">
+                <label htmlFor="password">パスワード</label>
+                <input
+                  id="password"
+                  type="password"
+                  className="border rounded-md p-2 text-sm"
+                  {...register("password")}
+                />
+                {errors["password"] && (
+                  <p className="text-red-500">{errors["password"].message}</p>
+                )}
+              </div>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white px-2 py-2 mt-4 rounded-md w-2/5"
+                >
+                  ログイン
+                </button>
+              </div>
+            </fieldset>
+          </form>
+        </>
+      )}
     </div>
   );
 };
